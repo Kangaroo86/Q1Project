1d91c8508f3886a515a556f8776839e2f83477f9
Cang Le <cang.b.le@gmail.com>
2017-08-11 09:15:17 -0700

    Q1ProjectIndex

--- src/index.js
+++ src/index.js
@@ -12,16 +12,20 @@ function main() {
   const $root = document.getElementById('root');
   $root.appendChild(InputForm());
 
-  scraper
-    .scrape(
-      'http://localhost:5000/sources/NBAWebsiteGoldenStateWarriors.html'
-    )
-    .then(players => {
-      $root.appendChild(Widget(players));
+  const URL = document.getElementById('textarea1');
+  const $submit = document.getElementById('executeProgram');
 
+  $submit.addEventListener('click', () => {
+    event.preventDefault();
+
+    scraper
+      .scrape(
+        URL.value //this ran on localhost //'http://localhost:5000/sources/NBAWebsiteGoldenStateWarriors.html'
+      )
+      .then(myOBJ => {$root.appendChild(Widget(myOBJ));
     });
 
-
-
-
+  })
 }
+//http://localhost:5000/sources/NBAWebsiteGoldenStateWarriors.html
+//http://localhost:5000/sources/CharlotteHornetsWEB.html
